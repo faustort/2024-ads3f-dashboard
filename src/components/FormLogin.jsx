a3bab86f012bf8b875ed5cd6f7ef66bedd31bd26
Your Name <you@example.com>
2024-05-09 22:06:38 -0300

    chore: Update npm dependencies and add Bootstrap and React Router

---
+++ src/components/FormLogin.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+
+export default function FormLogin() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  return (
+    <Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            placeholder="Sua senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+        </Form.Group>
+        <Container className="d-flex justify-content-end">
+          <Button variant="primary" type="submit">
+            Entrar
+          </Button>
+        </Container>
+      </Form>
+    </Container>
+  );
+}
